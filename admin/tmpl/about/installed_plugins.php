dc05d0863313f67722fc2a8bcebbff1d9778ba11
/ <xavier.dano@heolween.com>
2026-07-08 08:47:31 +0200

    Add plugin filter links on about page

--- admin/tmpl/about/installed_plugins.php
+++ admin/tmpl/about/installed_plugins.php
@@ -3,6 +3,9 @@
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+
+$pluginNameFilter = (string) ($this->pluginNameFilter ?? '');
 ?>
 
 <div class="card mt-3">
@@ -28,6 +31,38 @@ use Joomla\CMS\Language\Text;
                     data-bs-parent="#cb-about-plugins-accordion"
                 >
                     <div class="accordion-body">
+                        <div class="js-stools mb-3">
+                            <div class="js-stools-container-bar">
+                                <div class="btn-toolbar" role="toolbar" aria-label="<?php echo htmlspecialchars(Text::_('COM_CONTENTBUILDERNG_PLUGIN_NAME_FILTER_LABEL'), ENT_QUOTES, 'UTF-8'); ?>">
+                                    <div class="filter-search-bar btn-group me-2">
+                                        <label for="filter_plugin_name" class="visually-hidden">
+                                            <?php echo Text::_('COM_CONTENTBUILDERNG_PLUGIN_NAME_FILTER_LABEL'); ?>
+                                        </label>
+                                        <input
+                                            type="text"
+                                            name="filter_plugin_name"
+                                            id="filter_plugin_name"
+                                            value="<?php echo htmlspecialchars($pluginNameFilter, ENT_QUOTES, 'UTF-8'); ?>"
+                                            class="form-control"
+                                            placeholder="<?php echo htmlspecialchars(Text::_('COM_CONTENTBUILDERNG_PLUGIN_NAME_FILTER_LABEL'), ENT_QUOTES, 'UTF-8'); ?>"
+                                        >
+                                    </div>
+                                    <div class="btn-group">
+                                        <button type="submit" class="btn btn-primary" aria-label="<?php echo htmlspecialchars(Text::_('JSEARCH_FILTER_SUBMIT'), ENT_QUOTES, 'UTF-8'); ?>">
+                                            <span class="icon-search" aria-hidden="true"></span>
+                                            <span class="visually-hidden"><?php echo Text::_('JSEARCH_FILTER_SUBMIT'); ?></span>
+                                        </button>
+                                        <button
+                                            type="button"
+                                            class="btn btn-primary"
+                                            onclick="document.getElementById('filter_plugin_name').value='';this.form.submit();"
+                                        >
+                                            <?php echo Text::_('JSEARCH_FILTER_CLEAR'); ?>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <?php if (empty($this->plugins)) : ?>
                             <div class="alert alert-info mb-0">
                                 <?php echo Text::_('COM_CONTENTBUILDERNG_PLUGINS_NOT_AVAILABLE'); ?>
@@ -47,10 +82,16 @@ use Joomla\CMS\Language\Text;
                                     </thead>
                                     <tbody>
                                     <?php foreach ($this->plugins as $plugin) : ?>
+                                        <?php
+                                        $pluginId = (int) ($plugin['id'] ?? 0);
+                                        $pluginEditUrl = Route::_('index.php?option=com_plugins&view=plugin&layout=edit&extension_id=' . $pluginId, false);
+                                        ?>
                                         <tr>
                                             <td>
-                                                <strong><?php echo htmlspecialchars((string) ($plugin['name'] ?? ''), ENT_QUOTES, 'UTF-8'); ?></strong>
-                                                <span class="text-muted small d-block">#<?php echo (int) ($plugin['id'] ?? 0); ?></span>
+                                                <a href="<?php echo htmlspecialchars($pluginEditUrl, ENT_QUOTES, 'UTF-8'); ?>">
+                                                    <strong><?php echo htmlspecialchars((string) ($plugin['name'] ?? ''), ENT_QUOTES, 'UTF-8'); ?></strong>
+                                                </a>
+                                                <span class="text-muted small d-block">#<?php echo $pluginId; ?></span>
                                             </td>
                                             <td><code><?php echo htmlspecialchars((string) ($plugin['group'] ?? ''), ENT_QUOTES, 'UTF-8'); ?></code></td>
                                             <td><code><?php echo htmlspecialchars((string) ($plugin['element'] ?? ''), ENT_QUOTES, 'UTF-8'); ?></code></td>
